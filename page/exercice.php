7ae9a683422e2c02f9af88b5020c4a7b66d71101
Vinícius N. Silva <48424278+vnszero@users.noreply.github.com>
2023-10-18 14:30:58 -0300

    navigation part1

--- page/exercice.php
+++ page/exercice.php
@@ -4,14 +4,18 @@
     <meta charset="UTF-8">
     <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="assets\css\style.css">
+    <link rel="stylesheet" href="assets\css\exercice.css">
     <title>Exercício</title>
 </head>
-<body>
+<body class="exercice">
     <header class="header">
         <h1>Curso PHP</h1>
         <h2>Visualização do Exercício</h2>
     </header>
-    
+    <nav class="navigation">
+        <a href="#" class="green">Sem Formatacao</a>
+        <a href="index.php" class="red">Voltar</a>
+    </nav>
     <main class="main">
         <div class="content">
             
