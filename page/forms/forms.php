590550891d3ef324d0e586390fc2a0ea5b4e2f5f
vnszero <vnszero@gmail.com>
2024-03-17 18:08:06 -0300

    forms error handling

--- page/forms/forms.php
+++ page/forms/forms.php
@@ -2,35 +2,104 @@
 <div class="title">Formulário</div>
 
 <h2>Cadastro</h2>
+
+<?php
+    if(count($_POST) > 0)
+    {
+        $errors = [];
+
+        // isset($_POST['name']) // construção equivalente
+        if(!filter_input(INPUT_POST, "name"))
+        {
+            $errors['name'] = "Nome é Obrigatório";
+        }
+
+        if(filter_input(INPUT_POST, "birth-day"))
+        {
+            $date = DateTime::createFromFormat('d/m/Y', $_POST['birth-day']);
+            if(!$date)
+            {
+                $errors['birth-day'] = "Data deve estar no padrão dd/mm/aaaa";
+            }
+        }
+
+        if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL))
+        {
+            $errors['email'] = "Email inválido";
+        }
+
+        if (!filter_var($_POST['site'], FILTER_VALIDATE_URL))
+        {
+            $errors['site'] = "Site inválido";
+        }
+
+        $childrenConfig = ["options" => ["min_range"=>0, "max_range"=>20]];
+        if (!filter_var($_POST['children'], FILTER_VALIDATE_INT, $childrenConfig) && $_POST['children'] != 0)
+        {
+            $errors['children'] = "Quantidade de filhos inválida";
+        }
+
+        $wageConfig = ["options" => ["decimal" => ',']];
+        if (!filter_var($_POST['wage'], FILTER_VALIDATE_FLOAT, $wageConfig))
+        {
+            $errors['wage'] = 'salário inválido';
+        }
+    }
+?>
+
+<?php foreach ($errors as $error): ?>
+    <div class="alert alert-danger" role="alert">
+        <?= $error ?>
+    </div>
+<?php endforeach ?>
+
 <form action="#" method="post">
     <div class="form-row">
-        <div class="form-group col-md-9">
+        <div class="form-group col-md-8">
             <label for="name">Nome</label>
-            <input type="text" class="form-control" id="name" name="name" placeholder="Nome" value="<?= $_POST['name'] ?>">
+            <input type="text" class="form-control <?= $errors['name'] ? 'is-invalid' : '' ?>" id="name" name="name" placeholder="Nome" value="<?= $_POST['name'] ?>">
+            <div class="invalid-feedback">
+                <?= $errors['name'] ?>
+            </div>
         </div>
-        <div class="form-group col-md-3">
+        <div class="form-group col-md-4">
             <label for="birth-day">Nascimento</label>
-            <input type="text" class="form-control" id="birth-day" name="birth-day" placeholder="Nascimento" value="<?= $_POST['birth-day'] ?>">
+            <input type="text" class="form-control <?= $errors['birth-day'] ? 'is-invalid' : '' ?>" id="birth-day" name="birth-day" placeholder="Nascimento" value="<?= $_POST['birth-day'] ?>">
+            <div class="invalid-feedback">
+                <?= $errors['birth-day'] ?>
+            </div>
         </div>
     </div>
     <div class="form-row">
         <div class="form-group col-md-6">
             <label for="email">Email</label>
-            <input type="text" class="form-control" id="email" name="email" placeholder="Email" value="<?= $_POST['email'] ?>">
+            <input type="text" class="form-control <?= $errors['email'] ? 'is-invalid' : '' ?>" id="email" name="email" placeholder="Email" value="<?= $_POST['email'] ?>">
+            <div class="invalid-feedback">
+                <?= $errors['email'] ?>
+            </div>
         </div>
         <div class="form-group col-md-6">
             <label for="site">Site</label>
-            <input type="text" class="form-control" id="site" name="site" placeholder="Site" value="<?= $_POST['site'] ?>">
+            <input type="text" class="form-control <?= $errors['site'] ? 'is-invalid' : '' ?>" id="site" name="site" placeholder="Site" value="<?= $_POST['site'] ?>">
+            <div class="invalid-feedback">
+                <?= $errors['site'] ?>
+            </div>
         </div>
     </div>
     <div class="form-row">
         <div class="form-group col-md-6">
             <label for="children">Qtde de filhos</label>
-            <input type="text" class="form-control" id="children" name="children" placeholder="Qtde de filhos" value="<?= $_POST['children'] ?>">
+            <input type="number" class="form-control <?= $errors['children'] ? 'is-invalid' : '' ?>" id="children" name="children" placeholder="Qtde de filhos" value="<?= $_POST['children'] ?>">
+            <div class="invalid-feedback">
+                <?= $errors['children'] ?>
+            </div>
         </div>
         <div class="form-group col-md-6">
             <label for="wage">Salário</label>
-            <input type="text" class="form-control" id="wage" name="wage" placeholder="Salário" value="<?= $_POST['wage'] ?>">
+            <input type="text" class="form-control <?= $errors['wage'] ? 'is-invalid' : '' ?>" id="wage" name="wage" placeholder="Salário" value="<?= $_POST['wage'] ?>">
+            <div class="invalid-feedback">
+                <?= $errors['wage'] ?>
+            </div>
         </div>
     </div>
     <button class="btn btn-primary btn-lg">Enviar</button>
