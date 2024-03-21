5d39a6b5e2ed8f137331dd9ee68d25ef21844404
Vinícius N. Silva <48424278+vnszero@users.noreply.github.com>
2024-03-21 20:29:30 -0300

    pdo connection

---
+++ page/db_pdo/connection.php
@@ -0,0 +1,22 @@
+<?php
+    function newConnection($database = 'cod3r_database')
+    {
+        $server = '127.0.0.1:3306';
+        $user = 'root';
+        $drowssap = '';
+
+        try
+        {
+            $connection = new PDO("mysql:host=$server;dbname=$database",
+                            $user, $drowssap);
+            return $connection;
+        }
+        catch (PDOException $e)
+        {
+            die('Erro: ' . $e->getMessage());
+        }
+    }
+
+    newConnection();
+    echo "Fim!!!";
+?>
